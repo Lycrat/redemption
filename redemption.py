86f0f83203e187391bb235b8c2aca20d7b212dd7
Panya <panya.rattanarom@sky.uk>
2025-09-29 15:58:38 +0100

    MODIFY add main check

--- redemption.py
+++ redemption.py
@@ -1,27 +1,28 @@
 # list comprehension
 powers = [x**x for x in range(5)]
-print(powers)
 
 #Unicode chars
 unicode_str = "\u2665"
-print(unicode_str)
 
 #String splitting
 welcome_panya = "hello panya"
 arr_welcome_panya = welcome_panya.split()
-print(arr_welcome_panya)
 
 #String splicing
 welcome_bryan = "hello bryan"
 sliced_welcome_bryan = welcome_bryan[:5]
-print(sliced_welcome_bryan)
 
 #String methods
 name = "Osterley"
 name_scream = name.upper()
-print(name_scream)
 
 #Functions
 def eat(name: str = "chocolate") -> str:
     return f"I want some {name}, miam miam..."
 
+if __name__ == "__main__":
+    print(powers)
+    print(unicode_str)
+    print(welcome_panya)
+    print(arr_welcome_panya)
+    print(eat())
